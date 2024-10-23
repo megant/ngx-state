4290ecca7c974023947a25ebc257d7031f7bbf9d
Tibor Nemeth <tnemeth@caredx.com>
2024-10-23 21:06:19 +0200

    Unneeded files removed

--- src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of ngx-states
- */
-
-export * from './lib/states/state';
-export * from './lib/states/array-state';
-export * from './lib/states/key-value-state';
-export * from './lib/states/key-values-state';
